737f9d6193f04f3bd5e143c0020e98b625c3df0f
mikezks <michael.zikes@intauria.com>
2020-03-13 11:22:40 +0100

    RxJS

--- apps/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
+++ apps/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
@@ -1,6 +1,9 @@
+import { HttpParams, HttpHeaders, HttpClient } from '@angular/common/http';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { timer, Observable, Subscription, Subject } from 'rxjs';
-import { tap, share, takeUntil } from 'rxjs/operators';
+import { tap, share, takeUntil, switchMap, debounceTime, filter, distinctUntilChanged } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
+import { Flight } from '@flight-workspace/flight-api';
 
 @Component({
   selector: 'flight-workspace-flight-typeahead',
@@ -13,11 +16,38 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
   timerSubscribtion: Subscription;
   destroy$ = new Subject<void>();
 
-  constructor() { }
+  control = new FormControl();
+  flights$: Observable<Flight[]>;
+  loading: boolean;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.rxjsDemo();
+
+    this.flights$ =
+      this.control.valueChanges
+        .pipe(
+          distinctUntilChanged(),
+          filter(from => from.length > 2),
+          debounceTime(300),
+          tap(() => this.loading = true),
+          switchMap(from => this.load(from)),
+          tap(() => this.loading = false)
+        );
   }
+
+  load(from: string): Observable<Flight[]>  {
+    const url = "http://www.angular.at/api/flight";
+
+    const params = new HttpParams()
+                        .set('from', from);
+
+    const headers = new HttpHeaders()
+                        .set('Accept', 'application/json');
+
+    return this.http.get<Flight[]>(url, {params, headers});
+  };
 
   rxjsDemo(): void {
     this.timer$ = timer(0, 1000)
